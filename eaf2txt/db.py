222c3451dc728f2eae3cef8be999bca3c09eb6d6
kalenkovich <e.kalenkovich@gmail.com>
2021-07-14 14:25:29 +0300

    add annotations

--- eaf2txt/db.py
+++ eaf2txt/db.py
@@ -1,6 +1,8 @@
 import sqlite3
 from pathlib import Path
 
+import pandas as pd
+
 create_processed_files_table = """
 CREATE TABLE ProcessedFiles
 (_id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -22,16 +24,40 @@ CREATE TABLE Annotation
 """
 
 
-def create_database(path_to_database: Path):
-    if path_to_database.exists():
-        raise FileExistsError(f'Can\'t create database: file {path_to_database} already exists.')
-
+def _open_connection(path_to_database: Path):
     connection = sqlite3.connect(path_to_database)
     connection.execute("PRAGMA foreign_keys = 1")
     cursor = connection.cursor()
 
+    return connection, cursor
+
+
+def create_database(path_to_database: Path):
+    if path_to_database.exists():
+        raise FileExistsError(f'Can\'t create database: file {path_to_database} already exists.')
+
+    connection, cursor = _open_connection(path_to_database)
+
     cursor.execute(create_processed_files_table)
     cursor.execute(create_annotation_table)
-
     connection.commit()
     connection.close()
+
+
+def add_annotations(path_to_database: Path, eaf_filename: str, annotations_df: pd.DataFrame):
+    if not path_to_database.exists():
+        raise FileNotFoundError(f'Database file {path_to_database} does not exist.')
+
+    connection, cursor = _open_connection(path_to_database)
+
+    try:
+        cursor.execute(f'INSERT INTO ProcessedFiles (filename) VALUES("{eaf_filename}")')
+        file_id = cursor.lastrowid
+    except sqlite3.IntegrityError as e:
+        raise type(e)(f'EAF file {eaf_filename} has been added to the database earlier')
+
+    # We haven't committed yet, so if anything goes wrong during the insert, the transaction will be rolled back.
+    annotations_df['file_id'] = file_id
+    annotations_df.to_sql('Annotations', con=connection, if_exists='append', index=False)
+
+    connection.commit()
--- eaf2txt/tests/test_db.py
+++ eaf2txt/tests/test_db.py
@@ -1,7 +1,11 @@
 import sqlite3
+from pathlib import Path
+
+import pandas as pd
 import pytest
 
-from ..db import create_database
+from ..db import create_database, add_annotations, _open_connection
+from ..eaf2txt import convert_eaf_to_data_frame
 
 
 def test_create_database(tmp_path):
@@ -22,3 +26,38 @@ def test_create_database(tmp_path):
     with pytest.raises(FileExistsError) as e:
         create_database(db_path)
     assert 'Can\'t create database: file ' in str(e.value)
+
+
+def test_add_annotations(tmp_path, request):
+    # TODO: move the path definition to tests/config.py or somewhere
+    source_eaf = Path(request.fspath.dirpath('5959-0GS0.eaf'))
+    annotations_df = convert_eaf_to_data_frame(source_eaf)
+    db_path = tmp_path / 'eaf2txt.sqlite'
+
+    # Fails reasonably when the database file does not exist
+    with pytest.raises(FileNotFoundError) as e:
+        add_annotations(path_to_database=db_path,
+                        eaf_filename=source_eaf.name, annotations_df=annotations_df)
+    assert str(e.value).startswith('Database file ') and str(e.value).endswith(' does not exist.')
+
+    # Create the database
+    create_database(db_path)
+
+    # Add and check annotations
+    add_annotations(path_to_database=db_path,
+                    eaf_filename=source_eaf.name, annotations_df=annotations_df)
+    connection, cursor = _open_connection(db_path)
+    select_query = """
+    SELECT * 
+    FROM ProcessedFiles
+    INNER JOIN Annotations on ProcessedFiles._id = Annotations.file_id;
+    """
+    annotations_from_db = pd.read_sql(select_query, con=connection)
+    assert (annotations_from_db.drop(columns=['_id', 'filename']) == annotations_df).all().all()
+    assert (annotations_from_db['filename'] == source_eaf.name).all()
+
+    # Check that they won't be inserted twice (as long as the filename is the same)
+    with pytest.raises(sqlite3.IntegrityError) as e:
+        add_annotations(path_to_database=db_path,
+                        eaf_filename=source_eaf.name, annotations_df=annotations_df)
+    assert str(e.value) == f'EAF file {source_eaf.name} has been added to the database earlier'
