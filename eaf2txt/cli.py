ac96b0d03e2bf7e12ef0eaf6a01506c60b609ccb
kalenkovich <e.kalenkovich@gmail.com>
2021-07-14 15:51:56 +0300

    create database from cli

--- eaf2txt/cli.py
+++ eaf2txt/cli.py
@@ -1,7 +1,12 @@
 import argparse
+import os
 from pathlib import Path
 
 from .eaf2txt import convert_eaf_to_txt
+from .db import create_database
+
+
+DB_PATH_ENV_VAR = 'EAF2TXT_DB_PATH'
 
 
 def main():
@@ -15,7 +20,27 @@ def main():
     parser.add_argument('--summary', action='store_const', const=True, default=False,
                         help="print a short summary after each conversion.")
 
+    # database-related arguments
+    parser.add_argument('--create-database', action='store_const', const=True, default=False,
+                        help=f"create an empty database at --database-path, {DB_PATH_ENV_VAR}, or current working "
+                             "directory.")
+
+
     args = parser.parse_args()
+
+    if args.create_database:
+        db_path = args.database_path or os.environ.get(DB_PATH_ENV_VAR)
+        if not db_path:
+            db_path = Path(os.getcwd()) / 'eaf2txt.sqlite'
+            print(f'Neither the --database-path argument nor the environment variable {DB_PATH_ENV_VAR} are present. '
+                  f'The database will be created at {db_path}. Print yes to proceed, anything else to cancel.')
+            answer = input()
+            if answer != 'yes':
+                return
+
+        print(f'Creating an empty database at {db_path}')
+        create_database(Path(db_path))
+        return
 
     assert bool(args.file) ^ bool(args.directory), 'Exactly one of --file or --directory must be specified.'
 
